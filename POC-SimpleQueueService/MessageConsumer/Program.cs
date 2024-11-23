a8f01e8c1e8ad8910994fc4c793022a5c6e249c5
Wladson Cedraz <wladylinux@gmail.com>
2024-11-23 12:58:15 -0300

    [Feat] Implementing a simple message consumer

--- POC-SimpleQueueService/MessageConsumer/Program.cs
+++ POC-SimpleQueueService/MessageConsumer/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+Console.WriteLine("[Consumer] - Starting consuming messages from SQS");
+
+// Creating a SQS Client, specific to the Sao Paulo region
+var sqsClient = new AmazonSQSClient(RegionEndpoint.SAEast1);
+
+// Creating a request to send a message to the queue
+var request = new ReceiveMessageRequest
+{
+    QueueUrl = "https://sqs.sa-east-1.amazonaws.com/992382800221/discoverylogservice"
+};
+
+// Monitoring messages from the SQS
+while (true)
+{
+    // Capture the messages from the queue specified in the request
+    var response = await sqsClient.ReceiveMessageAsync(request);
+
+    // Iterate over the messages received
+    foreach (var message in response.Messages)
+    {
+        Console.WriteLine($"[Consumer] - Received message: {message.Body}");
+
+        // Delete the message from the queue
+        await sqsClient.DeleteMessageAsync(request.QueueUrl, message.ReceiptHandle);
+    }
+}
--- POC-SimpleQueueService/MessageProducer/Program.cs
+++ POC-SimpleQueueService/MessageProducer/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+Console.WriteLine("[Producer] - Starting message process sending to SQS");
+
+// Creating a SQS Client, specific to the Sao Paulo region
+var sqsClient = new AmazonSQSClient(RegionEndpoint.SAEast1);
+
+// Creating a request to send a message to the queue
+var requestSendMessage = new SendMessageRequest
+{
+    QueueUrl = "https://sqs.sa-east-1.amazonaws.com/992382800221/discoverylogservice",
+    MessageBody = "[LOG] - O personagem foi morto por Spinx"
+};
+
+// Sending the message to the queue
+var response = await sqsClient.SendMessageAsync(requestSendMessage);
+response = await sqsClient.SendMessageAsync(requestSendMessage);
+response = await sqsClient.SendMessageAsync(requestSendMessage);
+response = await sqsClient.SendMessageAsync(requestSendMessage);
+response = await sqsClient.SendMessageAsync(requestSendMessage);
